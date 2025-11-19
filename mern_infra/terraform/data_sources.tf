f90b6efb2bb9b49c720f21d1b33407459d23617f
root <root@DESKTOP-BLPH810.localdomain>
2025-11-19 16:04:44 +0530

    my data

--- mern_infra/terraform/data_sources.tf
+++
@@ -1,3 +0,0 @@
-// data_sources.tf is deprecated in the current configuration.
-// Networking and S3 resources are now created by `networking.tf`.
-// The previous Option A data sources have been intentionally removed.
